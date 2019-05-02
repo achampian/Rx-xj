95a9f0b3583061e8866f881109a7f3723f91035d
achampian <49868314+achampian@users.noreply.github.com>
2019-05-02 14:04:15 +0800

    Add files via upload

---
+++ Rx-xj-master/vaiul_my/houtai/vaiul.sql
@@ -0,0 +1,42 @@
+SET NAMES UTF-8;
+DORP DATABASE IF EXISTS vaiul;
+CREATE DATEBASE vaiul CHARSET=UTF8;
+USE vaiul;
+CREATE TABLE homepage_slideshow();
+CREATE TABLE homepage_navigationbar();
+CREATE TABLE homepage_message()
+
+CREATE TABLE page1_slideshow();
+CREATE TABLE page1_navigtionber();
+CREATE TABLE page1_message();
+CREATE TABLE page1_gametable();
+
+CREATE TABLE page2_message();
+CREATE TABLE page2_gamepresentation();
+CREATE TABLE page2_gamevideo();
+CREATE TABLE page2_leaveword();
+CREATE TABLE page2_downloadlink();
+
+CREATE TABLE page3_message();
+CREATE TABLE page3_gametable();
+
+CREATE TABLE page4_message();
+CREATE TABLE page4_gametable();
+
+CREATE TABLE page5_message();
+CRETAE TABLE page5_gametable();
+
+CREATE TABLE page6_message();
+CREATE TABLE page6_gametable()
+
+CREATE TABLE page7_message();
+CREATE TABLE page7_gametable();
+
+CREATE TABLE page8_message();
+CREATE TABLE page8_gametable();
+
+CREATE TABLE gametable();
+CREATE TABLE gametable_famil();
+CREATE TABLE gamevideo();
+CREATE TABLE downloadlink();
+CREATE TABLE gamephoto();
